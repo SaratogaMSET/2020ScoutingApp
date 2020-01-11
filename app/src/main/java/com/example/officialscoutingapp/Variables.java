3a0bb4bf853660fa87c99a169f8ea135ed1c8e86
nidhimath <nidhi@mathihalli.com>
2020-01-10 20:22:46 -0800

    finished auto

---
+++ app/src/main/java/com/example/officialscoutingapp/Variables.java
@@ -0,0 +1,23 @@
+package com.example.officialscoutingapp;
+
+public class Variables {
+
+    public static String myTeamNumber = "";
+    public static String myMatchNumber = "0";
+    public static String myScouterName = "";
+    public static String alliance = "";
+    public static String tabNum = "";
+
+    public static int score = 0;
+
+    public static int innerPort = 0;
+    public static int outerPort = 0;
+    public static int bottomPort = 0;
+    public static boolean crossedLine = false;
+
+
+
+
+
+
+}
